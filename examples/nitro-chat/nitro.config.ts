7cfff9b4d4db3f9df7f2e8874df546d9691217f1
Rihan Arfan <me@file.properties>
2026-01-23 20:58:47 +0000

    fix: trace deps

--- examples/nitro-chat/nitro.config.ts
+++ examples/nitro-chat/nitro.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from "nitro";
 export default defineConfig({
   serverDir: "./server",
   compatibilityDate: "2025-01-20",
-  traceDeps: ["@discordjs/ws"],
+  traceDeps: [
+    "discord.js",
+    "@discordjs/ws",
+    "@discordjs/voice",
+    "zlib-sync",
+    "tslib",
+    "@microsoft/microsoft-graph-client"
+  ],
 });
